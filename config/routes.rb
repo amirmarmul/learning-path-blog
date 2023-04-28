e982eb6ddaabbc5b896fc16793b98079cfe512b2
Amir Marmul <amiruddinmarmul@gmail.com>
2023-04-28 15:23:18 +0800

    authenticating article admin pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :users
   resources :articles
 
   # Defines the root path route ("/")
