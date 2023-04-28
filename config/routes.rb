25615f80f9cc9383a3ff721899b9873e835d62e2
Amir Marmul <amiruddinmarmul@gmail.com>
2023-04-28 14:36:55 +0800

    adding destroy action

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get "/articles/:id", to: "articles#show", as: :article
   get "/articles/:id/edit", to: "articles#edit", as: :edit_article
   patch "/articles/:id", to: "articles#update"
+  delete "/articles/:id", to: "articles#destroy"
   post "/articles", to: "articles#create", as: :articles
 
   # Defines the root path route ("/")
